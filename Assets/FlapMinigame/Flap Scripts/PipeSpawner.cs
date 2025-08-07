5392d13d851b186690b804719f92586ff2ed520c
tyceo <129821253+tyceo@users.noreply.github.com>
2025-08-07 16:19:23 +1000

    added all minigames

--- Assets/FlapMinigame/Flap Scripts/PipeSpawner.cs
+++ Assets/FlapMinigame/Flap Scripts/PipeSpawner.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build;
+
 using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
--- Assets/PapertossMinigame/Scripts/PaperTossCollisions.cs
+++ Assets/PapertossMinigame/Scripts/PaperTossCollisions.cs
@@ -15,12 +15,13 @@ public class PaperTossCollisions : MonoBehaviour
         if (collision.gameObject.tag == "Goal")
         {
             Debug.Log("Score");
+            ScoreManager.Instance.AddScore(1);
 
             //add manager functions
 
 
 
-             GameObject clone =  Instantiate(paperballPrefab, new Vector3(-5.81f, 0, 0), Quaternion.identity);
+            GameObject clone =  Instantiate(paperballPrefab, new Vector3(-5.81f, 0, 0), Quaternion.identity);
 
             //Instantiate(paperballPrefab, spawnPos);
 
